3042c62c0037ef84f085d9e4699cef3e09b1bc7a
Emre Enercan <75279495+bugpoop@users.noreply.github.com>
2021-05-06 15:54:36 +0300

    Add files via upload

---
+++ WinFormsApp1/Bot.cs
@@ -0,0 +1,132 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public static class Bot
+    {
+        // setting the variables
+        public static string tckn;
+        public static string sifre;
+        public static IWebDriver driver;
+        public static string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+        public static void Times(this int count, Action action)
+        {
+            // repetition legitimizes.
+            for (int i = 0; i < count; i++)
+            {
+                action();
+            }
+        }
+        public static void launch()
+        {
+            // Setting the driver options.
+
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument($@"user-data-dir="+path+@"\Local\Google\Chrome\UserData\Default");
+            
+           //  launching the driver and checking the version           
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("chrome://version");
+
+            while (true)
+            {
+                int hour = DateTime.Now.Hour;
+                int minute = DateTime.Now.Minute;
+             
+                if (hour == 09 && minute > 30) 
+                { 
+                   break;
+                }
+                else
+                {
+                    Thread.Sleep(5);
+                };
+                
+            }
+
+            // logging in
+            driver.Navigate().GoToUrl("https://giris.eba.gov.tr/EBA_GIRIS/giris.jsp");
+            driver.FindElement(By.XPath("//*[@id=\"tckn\"]")).SendKeys(tckn);
+            driver.FindElement(By.XPath("//*[@id=\"password\"]")).SendKeys(sifre);
+            driver.FindElement(By.XPath("/html/body/div[2]/div/div[1]/div/div[2]/form/div[5]/button")).Click();
+
+
+// this is a terrible way of doing this. Too bad!
+
+            // joining the lesson
+            driver.Navigate().GoToUrl("https://eba.gov.tr/#/anasayfa");
+            driver.FindElement(By.XPath("/html/body/app-root/app-anasayfa-page/div[2]/div/div/div[1]/div[2]/div[3]/div[3]/a[2]")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath(("//*[@id=\"joinMeeting\"]"))).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.Id("join")).Click();
+            Thread.Sleep(605000);
+            // joining the lesson
+            driver.Navigate().GoToUrl("https://eba.gov.tr/#/anasayfa");
+            driver.FindElement(By.XPath("/html/body/app-root/app-anasayfa-page/div[2]/div/div/div[1]/div[2]/div[3]/div[3]/a[2]")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath(("//*[@id=\"joinMeeting\"]"))).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.Id("join")).Click();
+            Thread.Sleep(605000);
+            // joining the lesson
+            driver.Navigate().GoToUrl("https://eba.gov.tr/#/anasayfa");
+            driver.FindElement(By.XPath("/html/body/app-root/app-anasayfa-page/div[2]/div/div/div[1]/div[2]/div[3]/div[3]/a[2]")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath(("//*[@id=\"joinMeeting\"]"))).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.Id("join")).Click();
+            Thread.Sleep(605000);
+            // joining the lesson
+            driver.Navigate().GoToUrl("https://eba.gov.tr/#/anasayfa");
+            driver.FindElement(By.XPath("/html/body/app-root/app-anasayfa-page/div[2]/div/div/div[1]/div[2]/div[3]/div[3]/a[2]")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath(("//*[@id=\"joinMeeting\"]"))).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.Id("join")).Click();
+            Thread.Sleep(605000);
+            // joining the lesson
+            driver.Navigate().GoToUrl("https://eba.gov.tr/#/anasayfa");
+            driver.FindElement(By.XPath("/html/body/app-root/app-anasayfa-page/div[2]/div/div/div[1]/div[2]/div[3]/div[3]/a[2]")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath(("//*[@id=\"joinMeeting\"]"))).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.Id("join")).Click();
+            Thread.Sleep(605000);
+            // joining the lesson
+            driver.Navigate().GoToUrl("https://eba.gov.tr/#/anasayfa");
+            driver.FindElement(By.XPath("/html/body/app-root/app-anasayfa-page/div[2]/div/div/div[1]/div[2]/div[3]/div[3]/a[2]")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath(("//*[@id=\"joinMeeting\"]"))).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.Id("join")).Click();
+            Thread.Sleep(605000);
+            // joining the lesson
+            driver.Navigate().GoToUrl("https://eba.gov.tr/#/anasayfa");
+            driver.FindElement(By.XPath("/html/body/app-root/app-anasayfa-page/div[2]/div/div/div[1]/div[2]/div[3]/div[3]/a[2]")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath(("//*[@id=\"joinMeeting\"]"))).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.Id("join")).Click();
+            Thread.Sleep(605000);
+            // joining the lesson
+            driver.Navigate().GoToUrl("https://eba.gov.tr/#/anasayfa");
+            driver.FindElement(By.XPath("/html/body/app-root/app-anasayfa-page/div[2]/div/div/div[1]/div[2]/div[3]/div[3]/a[2]")).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath(("//*[@id=\"joinMeeting\"]"))).Click();
+            Thread.Sleep(3000);
+            driver.FindElement(By.Id("join")).Click();
+            Thread.Sleep(605000);
+
+
+        }
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,133 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bot.launch();
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try { Bot.driver.Close(); }
+            catch (System.NullReferenceException)
+            {
+                Close();
+            }
+            try { Bot.driver.Quit(); }
+            catch (System.NullReferenceException)
+            {
+                Close();
+            }
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private bool _dragging = false;
+        private Point _offset;
+        private Point _start_point = new Point(0, 0);
+
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _dragging = true;  // _dragging is your variable flag
+            _start_point = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _dragging = false;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_dragging)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this._start_point.X, p.Y - this._start_point.Y);
+            }
+        }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Bot.tckn = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Bot.sifre = textBox2.Text;
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            Bot.tckn = textBox1.Text;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+            Bot.sifre = textBox2.Text;
+        }
+
+        private void label4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
